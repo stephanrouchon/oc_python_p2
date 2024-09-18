95bc97411d55972732a02508f662681c20ae1129
Stephan Rouchon <stephanrouchon@gmail.com>
2024-09-18 10:58:15 +0200

    transformation des données sous forme de dictionnaire

--- main.py
+++ main.py
@@ -9,19 +9,24 @@ url = "https://books.toscrape.com/catalogue/tipping-the-velvet_999/index.html"
 def scrapbook(url):
     page = requests.get(url)
 
+
+
     soup = BeautifulSoup(page.content,"html.parser")
+    book={}
+    book["product_page_url"]=url
 
     #extraction du title du livre
-    book_title = soup.find('h1').get_text()
+    book["title"] = soup.find('h1').get_text()
 
     #extraction de l'url de l'image
     image = soup.find('img')
     image = image['src']
-    url_image = "https://books.toscrape.com/"+image.replace("../../","")
+    book["image_url"] = "https://books.toscrape.com/" + image.replace("../../", "")
+
 
     #extraction de la description du livre
     description = soup.find('div',id="product_description")
-    description = description.find_next('p').get_text()
+    book["product_description"] = description.find_next('p').get_text()
 
     #extraction des donnees du tableau du livre
     table = soup.find('table')
@@ -32,31 +37,31 @@ def scrapbook(url):
         if information != -1:
             informations.append(information.text)
 
+    book["price_including_tax"] = informations[3]
+    book["price_exluding_tax"] = informations[2]
+    book["universal_ product_code"] = informations[0]
+
     #récuperer la valeur numérique du stock dans la string et la convertir
     stock_text = re.findall(r'\d+',informations[5])
     stock = stock_text[0]
-    informations[5] = int(stock)
+    book["number_available"] = int(stock)
 
     #récuperer la class star rating
-    review_rating = soup.find('p', class_=re.compile(r'^star-rating')).get('class')[1]
+    book["review_rating"] = soup.find('p', class_=re.compile(r'^star-rating')).get('class')[1]
 
     #recuperer la catégorie du livre
     categorie = soup.find('li').find_next('li').find_next('li')
     categorie = categorie.get_text()
-    categorie = categorie.replace("\n","")
-
-   #Mettre dans une liste tous les élements  et dans le bonne ordre
-    book = [url,informations[0],book_title,informations[3],informations[4],
-           informations[5],description,categorie,review_rating,url_image]
-
+    book["category"] = categorie.replace("\n", "")
     return book
 
-livre=scrapbook(url)
+livre = scrapbook(url)
+
 
 #Création fichier csv
 with open('livres.csv','w') as fichier:
     writer = csv.writer(fichier, delimiter=',')
-    writer.writerow(livre)
+
 
 
 
